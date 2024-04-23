573fc549a96976ef01d55b3ee69db2fcd795d4fe
Rob Anderson <mctalian@no-reply.github.com>
2024-04-23 00:40:19 -0600

    Backdrop for loader

--- src/client/app/components/Loader.tsx
+++ src/client/app/components/Loader.tsx
@@ -1,25 +1,14 @@
-import { Box, CircularProgress } from "@mui/material";
+import { Backdrop, CircularProgress } from "@mui/material";
 import { useAppSelector } from "../store/hooks";
 
 export default function Loader() {
   const loading = useAppSelector((state) => state.loading.value);
-  if (loading) {
-    return (
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        position="fixed"
-        top={0}
-        left={0}
-        width="100%"
-        height="100%"
-        bgcolor="rgba(255, 255, 255, 0.7)"
-      >
-        <CircularProgress />
-      </Box>
-    )
-  } else {
-    return <></>
-  }
+  return (
+    <Backdrop
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={loading}
+    >
+      <CircularProgress />
+    </Backdrop>
+  )
 }
